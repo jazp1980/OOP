d7fd72ca813ae4b126c8d81ecaaa858512a6c5cf
jazp1980 <84363444+jazp1980@users.noreply.github.com>
2021-05-24 20:13:43 -0600

    importar todas las clases

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,10 +1,15 @@
 package com.ucreativa;
 
-import com.ucreativa.familia.Jorge;
+import com.ucreativa.familia.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
       Jorge bueno = new Jorge();
+        Rebeca esposa = new Rebeca("Hola");
     }
+
+
+
 }
